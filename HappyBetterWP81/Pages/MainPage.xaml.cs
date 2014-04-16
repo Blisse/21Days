6da34e008ebb3aee503b0063827081f30d3fa783
Victor Lai <lai.victor.vl@gmail.com>
2014-04-16 17:18:20 -0400

    Setting up date flyouts

--- HappyBetterWP81/Pages/MainPage.xaml.cs
+++ HappyBetterWP81/Pages/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -17,14 +18,37 @@ namespace HappyBetterWP81.Pages
 {
     public sealed partial class MainPage : Page
     {
+        private readonly CommandBar _addTodayAppBar = new CommandBar();
+        private readonly CommandBar _addDateOnlyAppBar = new CommandBar();
+
         public MainPage()
         {
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
+
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private void MainPagePivot_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var pivot = sender as Pivot;
+            if (pivot != null)
+            {
+
+            }
+        }
+
+        private void AddTodayAppBarButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddDatePickerFlyout_OnDatePicked(DatePickerFlyout sender, DatePickedEventArgs args)
         {
 
         }
